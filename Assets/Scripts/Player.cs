2adc11d2a1b00ddb93d0811c789b970e35fba91c
nidakul <nidakul@hotmail.com>
2023-08-12 01:47:16 +0300

    adding character script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 7f;
+    private void Update()
     {
-        
-    }
+        Vector2 inputVector = new Vector2(0, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1; 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
         
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
