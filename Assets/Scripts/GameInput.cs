87c2428418f16c553f9a680bcdd90c9b5dc56c69
nidakul <nidakul@hotmail.com>
2023-08-12 12:09:01 +0300

    input system refactor

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputActions playerInputActions;
+    private void Awake()
     {
-        
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 GetMovementVectorNormalized()
     {
-        
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+        return inputVector;
     }
 }
